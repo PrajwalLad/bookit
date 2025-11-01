c21217f436047e04cb18f7d0d8e132475c33068b
Prajwal Lad <157223038+PrajwalLad@users.noreply.github.com>
2025-11-01 17:33:10 +0530

    Update console log to use process.env.PORT

--- backend-bookit/index.js
+++ backend-bookit/index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 connectDB();
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
